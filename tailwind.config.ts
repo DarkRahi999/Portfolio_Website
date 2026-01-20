de06c03e9f6284e5d9e8fe6a3c6a399cd43fc601
DarkRahi999 <linnima072@gmail.com>
2026-01-21 02:27:38 +0600

    all base setup done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,47 @@
-import type { Config } from "tailwindcss";
+// import type { Config } from "tailwindcss";
 
-const config: Config = {
-	darkMode: "class",
-	content: [
-		"./src/**/*.{js,ts,jsx,tsx,mdx}",
-		"./app/**/*.{js,ts,jsx,tsx,mdx}",
-		"./components/**/*.{js,ts,jsx,tsx,mdx}",
-	],
-	theme: {
-		extend: {
-			screens: {
-				sd: "426px",
-			},
-		},
-	},
-	plugins: [require("tailwindcss-animate")],
+// const config: Config = {
+// 	darkMode: "class",
+// 	content: [
+// 		"./src/**/*.{js,ts,jsx,tsx,mdx}",
+// 		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+// 		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+// 	],
+// 	theme: {
+// 		extend: {
+// 			screens: {
+// 				sd: "426px",
+// 			},
+// 		},
+// 	},
+// 	plugins: [require("tailwindcss-animate")],
+// };
+
+// export default config;
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#050816",
+        secondary: "#aaa6c3",
+        tertiary: "#151030",
+        "black-100": "#100d25",
+        "black-200": "#090325",
+        "white-100": "#f3f3f3",
+      },
+      boxShadow: {
+        card: "0px 35px 120px -15px #211e35",
+      },
+      screens: {
+        xs: "450px",
+      },
+      backgroundImage: {
+        "hero-pattern": "url('/src/assets/herobg.png')",
+      },
+    },
+  },
+  plugins: [],
 };
-
-export default config;
